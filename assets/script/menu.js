12dc441771dd942278d04c65d24d03350b4ae18e
n3rdgir1 <nerdgirl11235813@gmail.com>
2016-12-12 23:27:42 -0600

    adds toggle to menu and inventory items displaying on page

---
+++ assets/script/menu.js
@@ -0,0 +1,27 @@
+function toggleItems(id) {
+  var active = document.getElementById(id);
+  active.className -= 'inactive';
+  $(`#${id}`).siblings()[0].className = 'inactive';
+}
+
+window.onload = function () {
+  var menu = document.getElementById('menu');
+  $.getJSON("https://n3rdgir1.github.io/cg_project_2/inventory.json", function(result){
+    $.each(result, function(i, item){
+      if(item.type == 'Cake' || item.type == 'Other') {
+        var div = document.createElement('div');
+        div.className = `item ${item.type}`;
+
+        var name = document.createElement('h3');
+        name.appendChild(document.createTextNode(item.name));
+        div.appendChild(name);
+
+        var content = document.createElement('p');
+        content.append(item.description);
+        div.appendChild(content);
+
+        menu.appendChild(div);
+      }
+    });
+  });
+}
